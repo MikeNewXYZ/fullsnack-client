31cd11dab2d2d5167f8a65a5137da9b803fa50e4
Feben-A <afa508165@gmail.com>
2024-06-07 12:17:38 +0100

    Add images, description and name from data

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+const main = document.querySelector("main");
+let snackArray = [];
+
+function getSnacks() {
+  fetch("http://localhost:5050/snacks")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      showSnacks(data);
+    });
+}
+
+getSnacks();
+
 // add votes function
 // search function
 
-
 function showSnacks(data) {
   main.innerHTML = "";
-
   snackArray = data.map((card) => {
-    const { name, image, description } = card;
+    const { snack_name, snack_description, image_url } = card;
+
+    const cardEl = document.createElement("div");
+    cardEl.classList.add("card");
 
     const cardImage = document.createElement("img");
-    cardImage.src = image;
-    cardImage.alt = name;
+    cardImage.src = image_url;
 
     cardEl.appendChild(cardImage);
 
@@ -20,27 +34,34 @@ function showSnacks(data) {
 
     const snackName = document.createElement("h1");
     snackName.classList.add("snack-name");
-    cardDescription.appendChild("snackName");
+    snackName.innerHTML = `${snack_name}`;
+    cardDescription.appendChild(snackName);
 
     const snackDescription = document.createElement("p");
     snackDescription.classList.add("snack-description");
-    cardDescription.appendChild("snackDescription");
+    snackDescription.innerHTML = `${snack_description}`;
+    cardDescription.appendChild(snackDescription);
+
+    const buttons = document.createElement("div");
+    buttons.classList.add("buttons");
+    snackDescription.appendChild(buttons);
 
     const addButton = document.createElement("button");
     addButton.classList.add("add-button");
-    cardDescription.appendChild(addButton);
+    buttons.appendChild(addButton);
 
-    addButton.addEventListener("click", addToCart());
+    const likeButton = document.createElement("button");
+    likeButton.classList.add("like-button");
+    buttons.appendChild(likeButton);
+
+    const likeIcon = document.createElement("i");
+    likeIcon.classList.add("fa-solid", "fa-heart");
+    likeButton.appendChild(likeIcon);
+
+    main.appendChild(cardEl);
+    return { snack_name, snack_description, image_url };
+
+    // addButton.addEventListener("click", addToCart());
+    //likeIcon.addEventListener("click", addToVotes());
   });
 }
-
-function getSnacks() {
-  fetch("")
-    .then((res) => res.json())
-    .then((data) => {
-      showSnacks(data);
-    });
-}
-
-const main = document.querySelector(main);
-const cardEl = document.querySelector(".card");
