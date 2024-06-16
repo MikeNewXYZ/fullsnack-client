d6bde6216cb2ddbeb658994c44f193934a8624a5
Mike Newberry <hello@mikenew.xyz>
2024-06-16 14:06:48 +0100

    add cart item list

--- site/scripts/components.js
+++ site/scripts/components.js
@@ -197,3 +197,92 @@ customElements.define(
 		}
 	},
 );
+
+customElements.define(
+	"cart-item-layout",
+	class extends HTMLElement {
+		constructor() {
+			super();
+			this.itemTemplate = this.querySelector("template");
+			this.listWrapperEl = this.querySelector("#cart-item-list-wrapper");
+			this.cart = {};
+		}
+
+		connectedCallback() {
+			this.cart = JSON.parse(localStorage.getItem("cart")) || {};
+			this.populateWithItems();
+		}
+
+		populateWithItems() {
+			this.listWrapperEl.innerHTML = "";
+
+			this.cart.forEach((data) => {
+				const itemClone = this.itemTemplate.content.cloneNode(true);
+				const containerEl = itemClone.getElementById("card-item-list");
+				containerEl.dataset.data = JSON.stringify(data);
+
+				this.listWrapperEl.appendChild(itemClone);
+			});
+		}
+	},
+);
+
+customElements.define(
+	"cart-item-list",
+	class extends HTMLElement {
+		constructor() {
+			super();
+			this.cart = {};
+			this.data = {};
+			this.imageEl = this.querySelector("#cart-item-image");
+			this.titleEl = this.querySelector("#cart-item-title");
+			this.vegetarianEl = this.querySelector("#cart-item-vegetarian");
+			this.healthyEl = this.querySelector("#cart-item-healthy");
+			this.removeEl = this.querySelector("#cart-item-remove");
+			this.quantityEl = this.querySelector("#cart-item-quantity-input");
+		}
+
+		connectedCallback() {
+			this.cart = JSON.parse(localStorage.getItem("cart")) || {};
+			this.data = JSON.parse(this.getAttribute("data-data")) || {};
+			this.populateWithData();
+
+			this.removeEl.addEventListener("click", this.handleRemove.bind(this));
+			this.quantityEl.addEventListener("input", this.handleQuantity.bind(this));
+		}
+
+		populateWithData() {
+			const { healthy, image_url, snack_name, vegetarian } = this.data.productData;
+
+			this.imageEl.src = image_url;
+			this.titleEl.textContent = snack_name;
+			this.vegetarianEl.textContent = vegetarian === 1 ? "i'm vegetarian" : "i'm not vegetarian";
+			this.healthyEl.textContent = healthy === 1 ? "i'm healthy" : "i'm not healthy";
+			this.quantityEl.value = this.data.quantity;
+		}
+
+		handleQuantity(e) {
+			const quantity = parseInt(e.target.value);
+
+			if (quantity <= 0) {
+				this.handleRemove();
+				return;
+			}
+
+			this.cart = this.cart.map((data) => ({
+				...data,
+				quantity: this.data.id === data.id ? quantity : data.quantity,
+			}));
+
+			localStorage.setItem("cart", JSON.stringify(this.cart));
+		}
+
+		handleRemove() {
+			const removeId = this.data.id;
+			this.cart = this.cart.filter(({ id }) => id !== removeId);
+			localStorage.setItem("cart", JSON.stringify(this.cart));
+
+			location.reload();
+		}
+	},
+);
